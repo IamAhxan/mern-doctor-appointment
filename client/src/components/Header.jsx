d95526c5349b02b5d6b8e6b26d22f3c388c9d92c
IamAhxan <mohammadahsan7744@gmail.com>
2025-11-03 20:39:02 +0500

    Home>Speciality Menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,11 +5,12 @@ const Header = () => {
     return (
         <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20'>
             {/* --------LEFT SIDE -------------- */}
-            <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vh] md:mv-[-30px]'>
+            <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
                 <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
-                    BOOK APPOINTMENT <br /> WITH TRUSTED DOCTORS
+                    Book Appointment<br />With Trusted Doctors
                 </p>
-                <div className='flex flex-col md:flex-row items-center gap3 text-white text-sm font-light'>
+
+                <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
                     <img className='w-28' src={assets.group_profiles} alt="" />
                     <p>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block' />
                         schedule your appointment hassle-free.
