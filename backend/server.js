374fb6ddffe00adf106a70dcae94635be859418d
IamAhxan <mohammadahsan7744@gmail.com>
2025-11-11 23:51:10 +0500

    backend server created

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+// APP CONFIG
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middleware
+
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.get('/', (req, res) => {
+    res.send('Hello from PRESCRIPTO backend!');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
